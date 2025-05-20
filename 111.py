2ef432a65fa1afbb546f02a01bdada018466b4e9
MidNight2436 <evgenijen1234@mail.ru>
2025-05-21 00:14:43 +0300

    1

--- 111.py
+++ 111.py
@@ -1,1 +1,18 @@
-print('test')
+nums = [1, 2, 3, 4, 5, 6, 7, 8]
+print(nums)
+#del nums[len(nums)-1]
+#print(nums)
+del nums[-1]
+print(nums)
+
+print(list(reversed(nums)))
+
+nums = [2*k+1 for k in range(7)]
+print(nums)
+print(sorted(nums, reverse=True))
+
+numbers = list(range(1, 10,))
+print(numbers)
+
+for number in range(1, 10):
+    print(number)
--- 123.py
+++ 123.py
@@ -1,1 +1,6 @@
-123
+def sqsum(n):
+    nums = [k*k for k in range(1, n+1)]
+    return sum(nums)
+m = 10
+print('Сумма квадратов чисел от 1 до', str(m)+':', sqsum(m))
+
