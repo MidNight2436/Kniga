a70624642851bcd23b955f4de4b9b403e4dcda4a
evgen <evgenijen1234@mail.ru>
2025-06-13 14:21:21 +0300

    ИИ

--- Глава 1/10.py
+++ Глава 1/10.py
@@ -14,3 +14,10 @@ def summa():
 
 print(spisok)
 print(summa())
+
+
+
+n = int(input("Введите длину списка "))
+spisok = list(range(1, n+1))
+print(spisok)
+print(sum(i for i in spisok if i % 2 == 1))
