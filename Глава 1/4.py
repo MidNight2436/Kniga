637d4d0839a74af1c3dedaf77018be6ad70f16f9
evgen <evgenijen1234@mail.ru>
2025-06-13 15:11:56 +0300

    ИИ

--- Глава 1/4.py
+++ Глава 1/4.py
@@ -4,3 +4,9 @@ for i in range(0, count + 1):
     x = 2 ** i
     spisok.append(x)
 print(f'Список степеней двойки {spisok}')
+
+
+
+count = int(input('Введите количество степеней двойки '))
+spisok = [2**i for i in range(count + 1)]
+print(f'Список степеней двойки {spisok}')
